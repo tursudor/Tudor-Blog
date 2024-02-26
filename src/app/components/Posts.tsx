0c6ea23cfee1f227ccc046acfdd7250daff1227d
Tudor Domocos <tudor.domocos@yahoo.com>
2024-02-26 11:43:38 +0000

    posts rendered and date formatted

--- src/app/components/Posts.tsx
+++ src/app/components/Posts.tsx
@@ -1,4 +1,5 @@
 import { getSortedPostsData } from "@/lib/posts"
+import ListItem from "./ListItem"
 
 export default function Posts() {
   const posts = getSortedPostsData()
@@ -10,7 +11,7 @@ export default function Posts() {
       </h2>
       <ul className="w-full">
         {posts.map(post => (
-          JSON.stringify(post)
+          <ListItem key={post.id} post={post} />
         ))}
       </ul>
     </section>
